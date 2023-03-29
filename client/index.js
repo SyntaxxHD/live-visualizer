71ee94935c4d6f9c282c242936c90a50046c576e
SyntaxxHD <syntaxhdyt@gmail.com>
2023-03-29 13:41:52 +0200

    window fix

--- client/index.js
+++ client/index.js
@@ -1,13 +1,21 @@
-const { app, BrowserWindow, globalShortcut } = require('electron');
+const {app, BrowserWindow, globalShortcut, desktopCapturer} = require('electron');
 
 app.on('ready', () => {
   let visualizerWindow = new BrowserWindow({
-    fullscreen: true
+    fullscreen: false
   })
 
+  desktopCapturer.getSources({
+    types: ['audio']
+  })
+  .then(async sources => {
+    for (const source of sources) {
+      console.log(source.name)
+    }
+  })
 
   visualizerWindow.setMenuBarVisibility(false)
-  visualizerWindow.loadFile('index.html')
+  visualizerWindow.loadFile('visualizer.html')
 
   for (let i = 1; i <= 9; i++) {
     globalShortcut.register(`CommandOrControl+${i}`, () => {
@@ -21,10 +29,10 @@ app.on('ready', () => {
 
   function moveWindowToScreen(index) {
     const screens = require('electron').screen.getAllDisplays();
-  
+
     if (index >= 0 && index < screens.length) {
       const screen = screens[index]
-  
+
       visualizerWindow.setBounds(screen.bounds)
     }
   }
