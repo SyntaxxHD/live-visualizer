b010c65e72b5f862a08fdf2e6541ed8f16a8a885
Leonard Mueller <leonard.mueller@sap.com>
2023-03-30 19:51:11 +0200

    add desktop stream and mic stream

--- client/spectrum.js
+++ client/spectrum.js
@@ -1,15 +1,37 @@
 const { ipcRenderer } = window;
 
-(async function() {
-	const constraint = await ipcRenderer.invoke('audio-source')
-  setStream(constraint)
-})();
+// (async function() {
+// 	const constraint = await ipcRenderer.invoke('audio-source')
+//   setStream(constraints)
+// })();
 
-async function setStream(constraint) {
-  const stream = await navigator.mediaDevices.getUserMedia(constraint)
+async function setStream(constraints) {
+  const stream = await navigator.mediaDevices.getUserMedia(constraints)
 
-  const mediaElement = document.querySelector('video')
+  const mediaElement = document.querySelector('audio')
   mediaElement.srcObject = stream
   mediaElement.play()
-  console.log(2)
 }
+
+async function setMicStream() {
+  const devices = await navigator.mediaDevices.enumerateDevices()
+
+  const microphones = devices.filter(device => device.kind === 'audioinput');
+  console.log(microphones)
+
+  const constraints = {
+    audio: {
+      deviceId: {
+        exact: 'fae2a7384af2aed55a6df41019102c52697f812f4ae3ff335f620cf3d97d248e'
+      }
+    }
+  }
+
+  const stream = await navigator.mediaDevices.getUserMedia(constraints)
+  
+  const mediaElement = document.querySelector('audio')
+  mediaElement.srcObject = stream
+  mediaElement.play()
+}
+
+setMicStream()
