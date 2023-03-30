9ad1a08aac47d3922ce1e7704b8cb4b74c22d885
SyntaxxHD <syntaxhdyt@gmail.com>
2023-03-30 23:15:16 +0200

    create fft data

--- client/spectrum.js
+++ client/spectrum.js
@@ -5,12 +5,10 @@ const { ipcRenderer } = window;
 //   setStream(constraints)
 // })();
 
-async function setStream(constraints) {
+async function setDesktopStream() {
+  const constraints = await ipcRenderer.invoke('audio-source')
   const stream = await navigator.mediaDevices.getUserMedia(constraints)
-
-  const mediaElement = document.querySelector('audio')
-  mediaElement.srcObject = stream
-  mediaElement.play()
+  createAudioContext(stream)
 }
 
 async function setMicStream() {
@@ -34,4 +32,27 @@ async function setMicStream() {
   mediaElement.play()
 }
 
-setMicStream()
+function createAudioContext(stream) {
+  const audioContext = new AudioContext()
+  const mediaStreamSource = audioContext.createMediaStreamSource(stream)
+  const analyser = audioContext.createAnalyser()
+  mediaStreamSource.connect(analyser)
+  analyser.fftSize = 2048
+
+  return {audioContext, analyser}
+}
+
+function createFFTData(audioContext, analyser) {
+  const frequencyData = new Uint8Array(analyser.frequencyBinCount)
+  requestAnimationFrame(() => createFFTData(audioContext, analyser))
+
+  analyser.getByteFrequencyData(frequencyData)
+
+  const fftData = new Uint8Array(128)
+  fftData.set(frequencyData.subarray(0, 32), 0)
+  fftData.set(frequencyData.subarray(64, 96), 32)
+  fftData.set(frequencyData.subarray(32, 64), 64)
+  fftData.set(frequencyData.subarray(96, 128), 96)
+}
+
+setDesktopStream()
