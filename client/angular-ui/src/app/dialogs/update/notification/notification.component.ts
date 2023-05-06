7c48897c755be2c700fcceb7091eb80c3e8bb048
SyntaxxHD <syntaxhdyt@gmail.com>
2023-05-06 17:55:23 +0200

    Fix download progress

--- client/angular-ui/src/app/dialogs/update/notification/notification.component.ts
+++ client/angular-ui/src/app/dialogs/update/notification/notification.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { IpcRenderer } from 'electron';
 import { UpdateErrorDialogComponent } from '../error/error.component';
@@ -13,19 +13,20 @@ declare const ipcRenderer: IpcRenderer
   styleUrls: ['./notification.component.sass']
 })
 export class UpdateNotificationDialogComponent {
-  constructor(public dialog: MatDialog) {}
-  
+  constructor(public dialog: MatDialog, private ngZone: NgZone) { }
+
   downloadUpdate(): void {
-    const dialogRef = this.dialog.open(UpdateProgressDialogComponent, {
-      disableClose: true
+    let dialogRef: any
+    this.ngZone.run(() => {
+      dialogRef = this.dialog.open(UpdateProgressDialogComponent, {
+        disableClose: true
+      })
     })
-    
-    ipcRenderer.send('ui.update.download.start')
 
     ipcRenderer.on('ui.update.download.stop', (event: Event) => {
       dialogRef.close()
     })
-    
+
     ipcRenderer.on('ui.update.error', (event: Event) => {
       dialogRef.close()
       this.dialog.open(UpdateErrorDialogComponent)
--- client/angular-ui/src/app/dialogs/update/progress/progress.component.ts
+++ client/angular-ui/src/app/dialogs/update/progress/progress.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, NgZone } from '@angular/core';
 import { ProgressBarMode } from '@angular/material/progress-bar';
 import { IpcRenderer } from 'electron';
 
@@ -10,14 +10,19 @@ declare const ipcRenderer: IpcRenderer
   styleUrls: ['./progress.component.sass']
 })
 export class UpdateProgressDialogComponent {
-  mode: ProgressBarMode = 'query'
+  constructor(private ngZone: NgZone, private cdr: ChangeDetectorRef) {}
   progress: number = 0
+  mode: ProgressBarMode = 'query'
 
   ngOnInit(): void {
+    ipcRenderer.send('ui.update.download.start')
     ipcRenderer.on('ui.update.progress', (event: Event, percent: number) => {
       if (this.mode === 'query') this.mode = 'determinate'
-
       this.progress = percent
+
+      this.ngZone.run(() => {
+        this.cdr.detectChanges();
+      });
     })
   }
 }
